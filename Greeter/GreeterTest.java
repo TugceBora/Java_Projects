1c7ced78f328239f8c0aadf8dab577cbd479b357
Tugce Bora <39812265+TugceBora@users.noreply.github.com>
2019-03-14 12:46:18 -0700

    Add files via upload

---
+++ Greeter/GreeterTest.java
@@ -0,0 +1,11 @@
+public class GreeterTest {
+    public static void main (String[] args){
+        Greeter.testGreeting ("tugce");
+        // Greeter g = new Greeter();
+
+        // String greeting = g.greet();
+        // g.greet("24");
+        // String greetingWithName = g.greet("Tugce" );
+        // System.out.println(greetingWithName);
+    }
+}
